8cbcc5bea7f5c423e43d05facefe572ad16de2c1
EPICmynamesBG <mynamesbg@gmail.com>
2017-08-13 14:46:29 -0400

    Progress - formatting successful, thought buggy

--- main.js
+++ main.js
@@ -7,7 +7,12 @@ define(function (require, exports, module) {
         MainViewManager     = brackets.getModule("view/MainViewManager"),
         FileUtils           = brackets.getModule("file/FileUtils"),
         DocumentManager     = brackets.getModule("document/DocumentManager"),
+        AppInit            = brackets.getModule('utils/AppInit'),
         path                = ExtensionUtils.getModulePath(module);
+
+    //////////////////////////////////////////////////////////////
+    //////  Add Language to CodeMirror and Language Manager //////
+    //////////////////////////////////////////////////////////////
 
     ExtensionUtils.addLinkedStyleSheet(path + "csv-styles.css");
 
@@ -15,32 +20,19 @@ define(function (require, exports, module) {
         return {
             startState: function () {
                 return {
-                    inColumn: true
+                    column: 0
                 };
             },
             token: function (stream, state) {
-                // console.log(stream);
-                // let match = stream.match(/^(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g)
-                // console.log(match);
-                // if (!match) {
-                //   return null;
-                // } else  {
-                //   return 'column';
-                // }
-
-                if (!state.inColumn) {
-                    stream.next();
-                    state.inColumn = true;
-                    return null;
-                } else {
-                    if (stream.skipTo(',')) { // Quote found on this line
-                        stream.next();
-                        state.inColumn = false; // Clear flag
-                    } else {
-                        stream.skipToEnd(); // Rest of line is string
-                    }
-                    return "column"; // Token style
-                }
+              var classStr = "column column-" + state.column;
+              if (stream.skipTo(',')) { // Cell found on this line
+                  stream.next();
+                  state.column += 1;
+              } else {
+                stream.skipToEnd();
+                state.column = 0;
+              }
+              return classStr; // Token style
             }
         };
     });
@@ -53,13 +45,71 @@ define(function (require, exports, module) {
         console.log("Language " + language.getName() + " is now available!");
     });
 
-    MainViewManager.on("currentFileChange", () => {
-        let doc = DocumentManager.getCurrentDocument();
-        let ext = doc ? FileUtils.getFileExtension(doc.file.fullPath).toLowerCase() : "";
-        if (doc && /csv/.test(ext)) {
-            $('.CodeMirror-code').addClass('csv');
+    ///////////////////////////////////////////////////////////////////////
+    //////////       Page Load formatting                        //////////
+    ///////////////////////////////////////////////////////////////////////
+
+    $.fn.textWidth = function(){
+      var html_org = $(this).html();
+      var html_calc = '<span>' + html_org + '</span>';
+      $(this).html(html_calc);
+      var width = $(this).find('span:first').width();
+      $(this).html(html_org);
+      return width;
+    };
+
+    var _calculateColumnWidths = function() {
+      var index = 0;
+      var currentColumns = $('.cm-m-csv.cm-column.cm-column-' + index);
+      while (currentColumns.length > 0) {
+        var largestWidth = -1;
+        currentColumns.each(function () {
+          var thisWidth = $(this).textWidth();
+          if (thisWidth > largestWidth) {
+            largestWidth = thisWidth;
+          }
+        });
+        currentColumns.width(function(i, w) {
+          return largestWidth > w ? largestWidth : w;
+        });
+        index++;
+        currentColumns = $('.cm-m-csv.cm-column.cm-column-' + index);
+      }
+      return;
+    };
+
+    var isCSVFile = function() {
+      var doc = DocumentManager.getCurrentDocument();
+      var ext = doc ? FileUtils.getFileExtension(doc.file.fullPath).toLowerCase() : '';
+      return doc && /csv/.test(ext);
+    }
+
+    var viewManagerEventHandler = function () {
+        if (isCSVFile()) {
+          $('.CodeMirror').addClass('csv');
+          setTimeout(_calculateColumnWidths, 50);
         } else {
-            $('.CodeMirror-code').removeClass('csv');
+          $('.CodeMirror').removeClass('csv');
         }
+        return;
+    }
+
+    MainViewManager.on('currentFileChange', viewManagerEventHandler);
+    MainViewManager.on('activePaneChange', viewManagerEventHandler);
+    MainViewManager.on('paneLayoutChange', viewManagerEventHandler);
+    MainViewManager.on('paneCreate', viewManagerEventHandler);
+    MainViewManager.on('pandeDestroy', viewManagerEventHandler);
+
+    AppInit.appReady(function() {
+      DocumentManager.on('documentSaved', function() {
+        if (isCSVFile()) {
+          _calculateColumnWidths();
+        }
+      });
+      DocumentManager.on('documentRefreshed', function() {
+        if (isCSVFile()) {
+          setTimeout(_calculateColumnWidths, 50);
+        }
+      })
     });
 });
